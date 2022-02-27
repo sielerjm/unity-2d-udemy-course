0eb6076bf7161e547a26fa7fe9cf44fa84e83428
Michael Sieler <sielerjm@oregonstate.edu>
2022-02-27 12:26:58 -0800

    Session 2, lecture 22 - basic level layout

---
+++ Section_002/DeliveryDriver/Assets/Scripts/Driver.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float moveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float direction = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, direction, 0);
+
+
+
+    }
+}
