804e498347a7edcb68ed634c7fd7d019afc8aa12
Michael Sieler <sielerjm@oregonstate.edu>
2022-03-01 08:47:59 -0800

    Section_003 - Lecture 40 Invoke Delays

--- Section_003/SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ Section_003/SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+
+    [SerializeField] float loadDelay = 0.5f; // delay
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         {
             Debug.Log("You Crashed!");
+            Invoke("ReloadScene", loadDelay);  // delays reloading the scene by a set time
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);  // Loads scene at index 0
     }
 }
--- Section_003/SnowBoarder/Assets/Scripts/FinishLine.cs
+++ Section_003/SnowBoarder/Assets/Scripts/FinishLine.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+
+    [SerializeField] float loadDelay = 1f; // delay
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("You Finished!");
+        if (collision.tag == "Player")
+        {
+            Debug.Log("You Finished!");
+            Invoke("ReloadScene", loadDelay);  // delays reloading the scene by a set time
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);  // Loads scene at index 0
     }
 }
